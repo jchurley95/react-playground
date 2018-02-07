581d2954ee7eee3a5794aadafe4b1379121056d0
jchurley95 <jchurley95@gmail.com>
2018-02-06 20:45:18 -0500

    Added second date picker to page

--- src/components/DatePickerPage/DatePicker/Tests/DatePicker.test.js
+++ src/components/DatePickerPage/DatePicker/Tests/DatePicker.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AccordionPage from './AccordionPage';
+import DatePicker from '../DatePicker';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<AccordionPage/>, div);
+    ReactDOM.render(<DatePicker/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
-
