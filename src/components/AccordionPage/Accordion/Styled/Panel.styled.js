212b79c9a71c6881ba92f10f50873adb01878944
jchurley95 <jchurley95@gmail.com>
2018-02-25 01:04:23 -0500

    Updated styling of accordion

--- src/components/AccordionPage/Accordion/Styled/Panel.styled.js
+++ src/components/AccordionPage/Accordion/Styled/Panel.styled.js
@@ -6,6 +6,11 @@ const PanelContainer = styled.div`
 const Header = styled.div`
     border: 1px solid black;
     padding: 5px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: rgba(100,200,220,1);
+    }
 `
 const Content = styled.div`
     border: 1px solid black;
